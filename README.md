967d3abaee7d2149e3349510f5650ca76b7fb0ba
YLStax <kan1.921017@gmail.com>
2019-03-14 01:29:28 +0900

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# react-flux-modules
+# React Flux Modules
+
+## Installation
+
+```
+npm install --save react-flux-modules
+```
+
+## Usage
+
+### ActionCreator
+
+```ts
+import { createActionCreator } from '../src/react-flux-modules';
+
+const fooActionCreator = createActionCreator<string>()('@TEST/FOO');
+```
+
+### ReducerCreator
+
+```ts
+import { createReducerCreator } from '../src/react-flux-modules';
+
+const fooReducerCreator = createReducerCreator<{ foo: string }>({ foo: '' })
+	.case(fooActionCreator, (state, action) => ({ ...state, foo: action.payload }));
+```
+
+### Store
+
+```ts
+import { useReducerCreator } from '../src/react-flux-modules';
+
+const { Provider, connect } = useReducerCreator(fooReducerCreator);
+```
+
+### Render
+
+```tsx
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+interface ITestProps {
+	foo: string
+	bar: () => void
+}
+
+const Test = (props: ITestProps) => (
+	<div>
+		<label>{props.foo}</label>
+		<button onClick={() => props.bar()}>bar</button>
+	</div>
+)
+
+const TestContainer = connect<ITestProps>(store => ({
+	foo: store.state.foo,
+	bar: () => store.dispatch(fooActionCreator('bar'))
+}))(Test)
+
+ReactDOM.render((
+	< Provider >
+		<TestContainer />
+	</Provider >
+), document.getElementById('test'));
+```
--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,13 +1,15 @@
-import { createActionCreator, createReducerCreator, createReducer } from '../src/react-flux-modules'
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { createActionCreator, createReducerCreator, createReducer, useReducerCreator } from '../src/react-flux-modules'
+
+const setIdActionCreator = createActionCreator<number>()('@TESTS/SETID')
+const setTextActionCreator = createActionCreator<{ text: string }>()('@TESTS/SETTEXT')
+const addIdActionCreator = createActionCreator()('@TESTS/ADDID')
 
 interface ITestState {
 	id: number
 	text: string
 }
-
-const setIdActionCreator = createActionCreator<number>()('@TESTS/SETID')
-const setTextActionCreator = createActionCreator<{ text: string }>()('@TESTS/SETTEXT')
-const addIdActionCreator = createActionCreator()('@TESTS/ADDID')
 
 const reducerCreator = createReducerCreator<ITestState>({ id: 1, text: 'init' })
 	.case(setIdActionCreator, (state, action) => ({ ...state, id: action.payload }))
@@ -15,6 +17,30 @@ const reducerCreator = createReducerCreator<ITestState>({ id: 1, text: 'init' })
 	.case(addIdActionCreator, state => ({ ...state, id: state.id + 1 }))
 
 const reducer = createReducer(reducerCreator)
+
+const fooActionCreator = createActionCreator<string>()('@TEST/FOO')
+
+const fooReducerCreator = createReducerCreator<{ foo: string }>({ foo: '' })
+	.case(fooActionCreator, (state, action) => ({ ...state, foo: action.payload }))
+
+const { Provider, connect } = useReducerCreator(fooReducerCreator)
+
+interface ITestProps {
+	foo: string
+	bar: () => void
+}
+
+const Test = (props: ITestProps) => (
+	<div>
+		<label>{props.foo}</label>
+		<button onClick={() => props.bar()}>bar</button>
+	</div>
+)
+
+const TestContainer = connect<ITestProps>(store => ({
+	foo: store.state.foo,
+	bar: () => store.dispatch(fooActionCreator('bar'))
+}))(Test)
 
 describe('action', () => {
 	it("setIdActionCreator.type toBe '@TESTS/SETID'", () => {
